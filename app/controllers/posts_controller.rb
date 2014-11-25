7ed68cd214409742a81d6dfc5fcc3cad854eec90
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-11-25 16:17:56 -0500

    wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,31 @@
 class PostsController < ApplicationController
-  before_filter :authenitcate_user!, except: [:index]
+  before_filter :authenitcate_user!, except: :index
 
   def index
-    @posts = Posts.all
+    @posts = Post.all
     @post = Post.new
   end
-  
+
+  def new
+    @post = Post.new
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = current_user_posts.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @post }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:link)
+  end
 end
