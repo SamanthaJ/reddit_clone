c82a1378e04aec670742c8e9ae2bcb82b78d7d09
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-11-25 15:33:26 -0500

    adds comments resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  before_filter :authenitcate_user!, except: [:index]
+
+  def index
+    @posts = Posts.all
+    @post = Post.new
+  end
+  
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :comment do
+    body "MyText"
+user_id 1
+post_id 1
+  end
+
+end
